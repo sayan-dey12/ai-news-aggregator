b80d71ed6c31088fe3714bdb535b2f5bf1cad927
SAYAN <sayandey1203@gmail.com>
2026-09-04 17:10:20 +0530

    YouTube channel's video searching by channel id and parse those video's info

---
+++ app/scrapers/youtube.py
@@ -0,0 +1,107 @@
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+import os
+
+import feedparser
+from pydantic import BaseModel
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api._errors import (
+    NoTranscriptFound,
+    TranscriptsDisabled,
+)
+from youtube_transcript_api.proxies import WebshareProxyConfig
+
+
+class Transcript(BaseModel):
+    text: str
+
+
+class ChannelVideo(BaseModel):
+    title: str
+    url: str
+    video_id: str
+    published_at: datetime
+    description: str
+    transcript: Optional[str] = None
+    
+    
+class YouTubeScraper:
+    
+    def __init__(self):
+        self.transcript_api = YouTubeTranscriptApi()
+        
+        
+    def _extract_video_id(self, video_url: str) -> str:
+        if "youtube.com/watch?v=" in video_url:
+            return video_url.split("v=")[1].split("&")[0]
+
+        if "youtube.com/shorts/" in video_url:
+            return video_url.split("shorts/")[1].split("?")[0]
+
+        if "youtu.be/" in video_url:
+            return video_url.split("youtu.be/")[1].split("?")[0]
+
+        return video_url
+        
+        
+    def _get_rss_url(self, channel_id: str) -> str:
+        return f"https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}"
+    
+    def get_latest_videos(
+        self,
+        channel_id: str,
+        hours: int = 24,
+    ) -> list[ChannelVideo]:
+
+        feed = feedparser.parse(
+            self._get_rss_url(channel_id)
+        )
+
+        if not feed.entries:
+            return []
+
+        cutoff_time = datetime.now(timezone.utc) - timedelta(hours=hours)
+
+        videos = []
+
+        for entry in feed.entries:
+
+            if "/shorts/" in entry.link:
+                continue
+
+            published_time = datetime(
+                *entry.published_parsed[:6],
+                tzinfo=timezone.utc,
+            )
+
+            if published_time >= cutoff_time:
+
+                video_id = self._extract_video_id(
+                    entry.link
+                )
+
+                videos.append(
+                    ChannelVideo(
+                        title=entry.title,
+                        url=entry.link,
+                        video_id=video_id,
+                        published_at=published_time,
+                        description=entry.get(
+                            "summary",
+                            "",
+                        ),
+                    )
+                )
+
+        return videos
+
+
+if __name__ == "__main__":
+    scraper = YouTubeScraper()
+
+    videos = scraper.get_latest_videos(
+        "UCNQ6FEtztATuaVhZKCY28Yw"
+    )
+
+    print(videos)
+        
